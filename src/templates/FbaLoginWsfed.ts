a5422726abdfb4d72a76c4abfeedc101c6f0fd21
Sergei Sergeev <sergeev.srg@gmail.com>
2017-12-27 09:10:31 +0300

    Fixed FBA issue. References #27

--- src/templates/FbaLoginWsfed.ts
+++ src/templates/FbaLoginWsfed.ts
@@ -1,5 +1,4 @@
-export const template = `
-  <?xml version="1.0" encoding="utf-8"?>
+export const template = `<?xml version="1.0" encoding="utf-8"?>
   <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     <soap:Body>
       <Login xmlns="http://schemas.microsoft.com/sharepoint/soap/">
